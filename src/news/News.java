f6d45b34a633ba65b3169d881fe762fc842e4533
peawyoyoyin <peawyoyoyin@gmail.com>
2016-12-09 16:35:05 +0700

    more random news

--- src/news/News.java
+++ src/news/News.java
@@ -1,12 +1,23 @@
 package news;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class News {
 	private String author;
 	private String content;
 	
+	public static List<News> randomNews = new ArrayList<>();
+	
+	static {
+		randomNews.add(new News("peawyoyoyin","progmeth final is too hard for me."));
+		randomNews.add(new News("roy lek","You call this an exam?"));
+		randomNews.add(new News("naisk133", "Hope I get Rank 1 on discrete again."));
+	}
+	
 	public static News getRandomNews() {
-		
-		return new News("roy lek","progmeth ez, discrete not");
+		return randomNews.get(new Random().nextInt(randomNews.size()));
 	}
 	
 	public News(String author) {
