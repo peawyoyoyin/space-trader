cf0eb6f39264e4bfce0d0150aa67ed5d4b6502d0
Roy <pawit2540@hotmail.com>
2016-12-12 21:11:39 +0700

    load font update pls

--- src/constants/ConfigConstant.java
+++ src/constants/ConfigConstant.java
@@ -1,5 +1,15 @@
 package constants;
 
+import java.io.File;
+import java.io.FilePermission;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.sun.javafx.tk.Toolkit;
+
 import javafx.scene.image.Image;
 import javafx.scene.media.AudioClip;
 import javafx.scene.text.Font;
@@ -12,7 +22,7 @@ public class ConfigConstant {
 	public static int startScreenWidth = 1120;
 	public static final Font START_SCREEN_FONT = Font.font("Verdana", FontPosture.REGULAR, 72);
 	public static final Font START_SCREEN_ITEM_FONT = Font.font("Verdana", FontPosture.REGULAR, 56);
-	
+
 	public static final Font GAME_FONT = Font.font("Verdana", FontPosture.REGULAR, 24);
 
 	public static final String UPTODATE_TEXT = "Game Client is Up to Date";
@@ -32,8 +42,9 @@ public class ConfigConstant {
 
 	public static class Resource {
 		public static AudioClip LASER_SOUND = new AudioClip(ClassLoader.getSystemResource("sfx_laser1.wav").toString());
-		
-		public static Image BULLET_IMAGE = new Image(ClassLoader.getSystemResource("Lasers/laserGreen12.png").toString());
+
+		public static Image BULLET_IMAGE = new Image(
+				ClassLoader.getSystemResource("Lasers/laserGreen12.png").toString());
 		public static Image SHIP_IMAGE = new Image(ClassLoader.getSystemResource("playerShip1_red.png").toString());
 		public static Image ENEMY_IMAGE = new Image(ClassLoader.getSystemResource("Enemies/enemyRed1.png").toString());
 		public static Image MAP_BACKGROUND = new Image(ClassLoader.getSystemResource("purple.png").toString());
@@ -48,7 +59,21 @@ public class ConfigConstant {
 				ClassLoader.getSystemResource("itemicon/itemweapon.png").toString());
 		public static Image ITEM_MATERIAL = new Image(
 				ClassLoader.getSystemResource("itemicon/itemmaterial.png").toString());
-		
-		public static Font HUD_FONT = Font.loadFont(ClassLoader.getSystemResource("kenvector_future.ttf").toString(), 15);
+
+		public static Font HUD_FONT = loadFont(ClassLoader.getSystemResource("kenvector_future.ttf").toString(), 15);
+
+	}
+
+	public static Font loadFont(String url, double size) {
+		try {
+			return Toolkit.getToolkit().getFontLoader().loadFont(new URL(url).openConnection().getInputStream(), size);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
